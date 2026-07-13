0c322d16a6243bee24936762a35a194535ee2b9c
Jacob Harris <jacobwho07@gmail.com>
2026-07-13 12:37:01 -0500

    added auth login_register context

--- src/auth/AuthContext.jsx
+++ src/auth/AuthContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState } from "react";
 
-const API = import.meta.env.API;
+const API = import.meta.env.VITE_API;
 
 const AuthContext = createContext();
 
@@ -8,11 +8,31 @@ export function AuthProvider({ children }) {
     const [token, setToken] = useState(null);
 
     const register = async (credentials) => {
-
-    }
+        const response = await fetch(API + "users/register",
+            {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(credentials)
+            });
+        const data = await response.json();
+        if (!response.ok) {
+            throw Error(result.message);
+        }
+        setToken(result.token);
+    };
 
     const login = async (credentials) => {
-
+        const response = await fetch(API + "/users/login",
+            {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify(credentials)
+            });
+            const result = await response.json();
+            if (!response.ok) {
+                throw Error(result.message);
+            }
+            setToken(result.token);
     }
 
     const logout = () => setToken(null);
