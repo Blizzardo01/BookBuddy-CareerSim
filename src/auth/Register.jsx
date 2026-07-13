e608040d463174d47509cd5515ae4869a3297a0e
Jacob Harris <jacobwho07@gmail.com>
2026-07-13 12:05:42 -0500

    added register functionality

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,9 +1,47 @@
+import { useState } from "react";
+import { useAuth } from "./AuthContext";
+import {Link} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+
 export default function Register() {
+    const { register } = useAuth();
+    const navigate = useNavigate();
+
+    const [error, setError] = useState(null);
+
+    const tryRegister = async (formData) => {
+        setError(null);
+
+        const email = formData.get("email");
+        const password = formData.get("password");
+        
+        try {
+            await register({ email, password });
+            navigate("/")
+        } catch (err) {
+            setError(err.message);    
+        }
+    };
 
 
     return (
         <>
         <h1>Register for an account</h1>
+        <form action={tryRegister}>
+            <label>
+                Email
+                <input type="text" name="username" required />
+            </label>
+            <label>
+                Password
+                <input type="password" name="password" required />
+            </label>
+            <button>Register</button>
+            {error && <p role="alert">{error}</p>}
+        </form>
+        <Link to="/login">
+            <p>Already have an account? Log in here.</p>
+        </Link>
         </>
     );
 }
